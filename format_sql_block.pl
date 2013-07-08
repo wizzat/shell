cedbe88bd19836984d1cdd6a2d625d547006c6e5
Mark Roberts <wizzat@gmail.com>
2013-07-08 10:31:17 -0700

    Basic shell utilities.  More to come, I'm sure

---
+++ format_sql_block.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl -w
+use strict;
+
+my @upper_words = qw/
+    %notfound
+    and
+    as
+    asc
+    auto_increment
+    begin
+    between
+    commit
+    cursor
+    date
+    datetime
+    declare
+    desc
+    distinct
+    double
+    else
+    elsif
+    end
+    fetch
+    float
+    from
+    function
+    if
+    in
+    insert
+    integer
+    into
+    is
+    loop
+    noparallel
+    not
+    null
+    number
+    or
+    out
+    package
+    procedure
+    return
+    rollback
+    select
+    set
+    smallint
+    sysdate
+    then
+    timestamp
+    update
+    values
+    varchar
+    varchar2
+    where
+    exists
+    using
+/;
+
+push(@upper_words,
+    'execute immediate',
+    'create table',
+    'create sequence',
+    'end if',
+    'delete from',
+    'exit when',
+    'group by',
+    'order by',
+    'exception when',
+    'case when',
+    'sql%rowcount',
+    'create or replace',
+    'package body',
+    'inner join',
+    'left outer join',
+    'on',
+    'drop table',
+    'drop sequence',
+    'reuse storage',
+    'truncate table',
+    'primary key',
+    'foreign key',
+    'alter table',
+    'add primary key',
+    'add column',
+);
+
+while (<STDIN>) {
+    $_ =~ s/\t/    /;
+    foreach my $word (@upper_words) {
+        $_ =~ s/\b$word\b/\U$word/ig;
+    }
+    print($_);
+}
