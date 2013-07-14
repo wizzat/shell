4dd58eafd15dd8c1562aaebd373e98c71e361992
Mark Roberts <wizzat@gmail.com>
2013-07-13 22:45:31 -0700

    find_up and find_up_dir now take default arguments.  find_unused_python has now been battle tested (again)

--- find_unused_python.sh
+++ find_unused_python.sh
@@ -1,1 +1,2 @@
-ack ‘def ‘ | perl -pe ‘s/.*def (\w+)\(.*/\1/’ | sort | uniq | xargs -I{} bash -c ‘echo -n {} && ack “\b{}\b” | wc -l’ | egrep -v ‘^test_’ | egrep ‘ 1$’
+#!/bin/bash
+ack 'def ' | perl -pe 's/.*def (\w+)\(.*/\1/' | sort | uniq | xargs -I{} bash -c 'echo -n {} && ack "\b{}\b" | wc -l' | egrep -v '^test_' | egrep ' 1$'
